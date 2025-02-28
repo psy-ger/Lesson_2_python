84bcc8a747c3b0b9731c7ea601a8655ca32bd0c3
Bohdan_Novytskyi <gruvoz.1234@gmail.com>
2025-02-28 20:24:52 +0200

    added v2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 number_user = int(input('Enter your number: '))
 
+#v1
 print(number_user // 1000)
 print(number_user % 1000 // 100)
 print(number_user % 100 // 10)
 print(number_user % 10)
+#v2
+for queue in str(number_user):
+    print(queue)
